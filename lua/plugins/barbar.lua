e830724aea228691ffb28aba23d23cdb254a44dd
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-08-18 23:15:19 +0800

    chore: do not change to insert mode after switch buffer

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -64,7 +64,7 @@ return {
 	opts = {
 		-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
 		-- animation = true,
-		-- insert_at_start = true,
+		insert_at_start = false,
 		-- …etc.
 	},
 	version = "^1.0.0", -- optional: only update when a new 1.x version is released
