c0410f30051c116f25a4190f5ece1e32edc79760
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-08-19 12:14:15 +0800

    fix: skip register kotlin_lsp lsp

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,12 +4,20 @@ local lspconfig = require("lspconfig")
 local mason_lspconfig = require("mason-lspconfig")
 
 local function hasProperty(object, prop)
-	return rawget(object, prop) ~= nil
+	return object[prop] ~= nil
 end
 
 -- auto setup installed lsp
 for _, server in pairs(mason_lspconfig.get_installed_servers()) do
-    if hasProperty(lspconfig, server) then
-        lspconfig[server].setup({})
-    end
+	-- if hasProperty(lspconfig, server) then
+	--     lspconfig[string.gsub(server, "-", "_")].setup({})
+	-- end
+
+	if server == "kotlin_lsp" then
+		-- skip register this server (this cannot be registered via lspconfig)
+		goto continue
+	end
+
+	lspconfig[server].setup({})
+	::continue::
 end
