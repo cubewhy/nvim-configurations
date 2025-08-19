335a60b41f8fba0f00a3e293efded54cfe015a84
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-08-19 12:17:06 +0800

    chore: add default config to rustaceanvim plugin

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -3,6 +3,44 @@ return {
 	version = "^6", -- Recommended
 	lazy = false, -- This plugin is already lazy
 
+	opts = {
+		server = {
+			default_settings = {
+				-- rust-analyzer language server configuration
+				["rust-analyzer"] = {
+					cargo = {
+						allFeatures = true,
+						loadOutDirsFromCheck = true,
+						buildScripts = {
+							enable = true,
+						},
+					},
+					-- Add clippy lints for Rust if using rust-analyzer
+					checkOnSave = diagnostics == "rust-analyzer",
+					-- Enable diagnostics if using rust-analyzer
+					diagnostics = {
+						enable = diagnostics == "rust-analyzer",
+					},
+					procMacro = {
+						enable = true,
+					},
+					files = {
+						excludeDirs = {
+							".direnv",
+							".git",
+							".github",
+							".gitlab",
+							"bin",
+							"node_modules",
+							"target",
+							"venv",
+							".venv",
+						},
+					},
+				},
+			},
+		},
+	},
 	config = function()
 		vim.g.rustaceanvim = {
 			server = {
