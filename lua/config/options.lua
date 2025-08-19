80d3fb30dfc739b16994cac15eae84cf8a418479
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-08-19 14:34:10 +0800

    fix: use mason-lspconfig to auto setup lsp

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,16 +8,16 @@ local function hasProperty(object, prop)
 end
 
 -- auto setup installed lsp
-for _, server in pairs(mason_lspconfig.get_installed_servers()) do
-	-- if hasProperty(lspconfig, server) then
-	--     lspconfig[string.gsub(server, "-", "_")].setup({})
-	-- end
-
-	if server == "kotlin_lsp" then
-		-- skip register this server (this cannot be registered via lspconfig)
-		goto continue
-	end
-
-	lspconfig[server].setup({})
-	::continue::
-end
+-- for _, server in pairs(mason_lspconfig.get_installed_servers()) do
+-- 	-- if hasProperty(lspconfig, server) then
+-- 	--     lspconfig[string.gsub(server, "-", "_")].setup({})
+-- 	-- end
+--
+-- 	if server == "kotlin_lsp" then
+-- 		-- skip register this server (this cannot be registered via lspconfig)
+-- 		goto continue
+-- 	end
+--
+-- 	lspconfig[server].setup({})
+-- 	::continue::
+-- end
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -34,7 +34,7 @@ return {
 	{
 		"mason-org/mason-lspconfig.nvim",
 		opts = {
-			automatic_enable = false,
+			automatic_enable = true,
 		},
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
