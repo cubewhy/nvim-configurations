29e918f5b3693d371ec39a95f6178c2276972cb9
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-08-24 08:17:53 +0800

    feat: edgy plugin

--- lua/config/options.lua
+++
@@ -1,9 +0,0 @@
-local lspconfig = require("lspconfig")
-
--- local mason = require("mason")
-local mason_lspconfig = require("mason-lspconfig")
-
-local function hasProperty(object, prop)
-	return object[prop] ~= nil
-end
-
