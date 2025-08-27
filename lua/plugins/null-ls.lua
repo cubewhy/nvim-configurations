cf1735ccf96a99dabad18116082071580182ea8f
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-08-27 13:00:11 +0800

    feat: add mason-null-ls plugin

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -18,4 +18,12 @@ return {
 			})
 		end,
 	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+	}  ,
 }
