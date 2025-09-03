653dc00f3ce16734471e38a6275ac38c30567391
cubewhy <61075476+cubewhy@users.noreply.github.com>
2025-09-03 21:35:33 +0800

    feat: icons in cmp

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,14 +1,30 @@
-vim.api.nvim_set_keymap("n", "<leader>e", ":Neotree toggle<cr>", { silent = true, noremap = true, desc = "Toggle file explorer" })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>e",
+	":Neotree toggle<cr>",
+	{ silent = true, noremap = true, desc = "Toggle file explorer" }
+)
 
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false, -- neo-tree will lazily load itself
-  }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		lazy = false,
+		opts = function(_, opts)
+			local function on_move(data)
+				Snacks.rename.on_rename_file(data.source, data.destination)
+			end
+			local events = require("neo-tree.events")
+			opts.event_handlers = opts.event_handlers or {}
+			vim.list_extend(opts.event_handlers, {
+				{ event = events.FILE_MOVED,   handler = on_move },
+				{ event = events.FILE_RENAMED, handler = on_move },
+			})
+		end,
+	},
 }
